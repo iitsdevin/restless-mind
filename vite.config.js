8a857a397b729897728af0c71a32626f58ff6f0c
iitsdevin <devin.earle@gmail.com>
2026-02-22 23:47:28 +0800

    Clean up comments in vite.config.js
    Remove comments about repository name in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // This must match your GitHub repository name exactly!
-  // If your repo is named something else, change 'restless-mind' to match.
   base: '/restless-mind/', 
 });
